56ec6b9dd3fdac1491c4ca893147f98c4befcb36
itsfabijano <fabian.ehringer@gmx.net>
2025-10-16 18:30:36 +0200

    update

--- zsh/.config/zsh/aliases.sh
+++ zsh/.config/zsh/aliases.sh
@@ -2,6 +2,7 @@ alias git-delete-merged='git branch --merged | grep -Ev "(^\*|master|main|dev)" 
 
 alias gcof='git checkout $(git branch --sort=-committerdate | fzf --reverse --height=20% --info=inline)'
 
-alias nixbox:start='utmctl start NixOS &> /dev/null && ssh -y nixbox.local'
-alias nixbox:pause='utmctl suspend NixOS'
-alias nixbox:stop='utmctl stop NixOS'
+alias nixbox:start='utmctl start NixOS &> /dev/null && ssh -y nixbox.local && utmctl suspend NixOS'
+alias nixbox:connect='ssh -y nixbox.local'
+alias nixbox:stop='utmctl suspend NixOS'
+alias nixbox:shutdown='utmctl stop NixOS'
